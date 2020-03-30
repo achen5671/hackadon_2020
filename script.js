37a17b0dca8b2daa6b391c1557506b9e1ed7fef6
achen5671 <44120813+achen5671@users.noreply.github.com>
2020-03-29 20:42:05 -0400

    checkpoint2

---
+++ script.js
@@ -0,0 +1,15 @@
+var slider= document.getElementById("slider");
+var slider2= document.getElementById("slider2");
+
+var val=document.getElementById("value");
+var val2=document.getElementById("value2");
+
+val.innerHTML = slider.value;
+val2.innerHTML = slider2.value;
+
+slider.oninput = function(){
+	val.innerHTML = this.value;
+}
+slider2.oninput = function(){
+	val2.innerHTML = this.value;
+}
